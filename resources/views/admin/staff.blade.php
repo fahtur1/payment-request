806a2c8d5c0bee5f73030f614c8ef55f746f5f51
fahtur1 <fahturrahman18ti@mahasiswa.pcr.ac.id>
2021-08-20 08:14:50 +0700

    test ssh work

--- resources/views/admin/staff.blade.php
+++ resources/views/admin/staff.blade.php
@@ -54,6 +54,7 @@
 
 @push('js')
 <script src="{{ asset('assets') }}/vendor/datatables/jquery.dataTables.min.js"></script>
+
 <script src="{{ asset('assets') }}/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
 <script src="{{ asset('assets') }}/js/demo/datatables-demo.js"></script>
